537aba62d356283a688bde947d68cace2df98e9f
jonghoon.park <hom8181@naver.com>
2021-09-05 19:03:27 +0900

    2021-09-05 Add Enum Ocr result

---
+++ src/main/java/kr/jhpark/ocr/domain/EnumOcrResultType.java
@@ -0,0 +1,11 @@
+package kr.jhpark.ocr.domain;
+
+public enum EnumOcrResultType {
+    // OCR 결과 fields
+    name,                       // 회사명
+    address,                    // 사업장 소재지
+    business_license_number,    // 사업자 등록 번호
+    representative_name,        // 대표자명
+    business_type,              // 업태
+    business_item               // 종목
+}
